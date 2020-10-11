2eeae5f775091248e428c55d428fddeb41c7ce92
Higor Viegas <viegas.higor@gmail.com>
2020-10-11 12:14:12 -0300

    Service layer, component registration

--- src/main/java/com/higor/course/repositories/UserRepository.java
+++ src/main/java/com/higor/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.higor.course.entities.User;
 
+//@Repository // não precisa pois extende o JpaRepository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	
